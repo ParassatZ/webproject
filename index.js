1a1dd94627f3fa0925b643f2249567e0772ae823
ParassatZ <krayx170804@gmail.com>
2022-12-19 01:44:00 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+productContainers.forEach((item,i) => {
+    let containerDimenstions=item.getBoundingClientRect();
+    let containerWidth=containerDimenstions.width;
+
+    nxtBtn[i].addEventListener('click',() => {
+        item.scrollLeft+=containerWidth;
+    })
+    preBtn[i].addEventListener('click',() => {
+        item.scrollLeft-=containerWidth;
+    })
+})
