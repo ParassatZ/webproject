70f61a59f16baf57e6619b7b1abd270df6a8da17
ParassatZ <116261535+ParassatZ@users.noreply.github.com>
2022-12-19 17:37:49 +0500

    Update newsPage.js

--- newsPage.js
+++ newsPage.js
@@ -26,3 +26,10 @@ productContainers.forEach((item,i) => {
         item.scrollLeft-=containerWidth;
     })
 })
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByClassName('links-container')[0]
+const navL = document.querySelectorAll('links-container li') ;
+
+toggleButton.addEventListener('click', () => {
+    navLinks.classList.toggle('active')
+  })
