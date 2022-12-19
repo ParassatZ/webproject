2daa92ac83eadae152f56d3ad7145344593d0e69
Adilet <210103442@stu.sdu.edu.kz>
2022-12-20 00:48:07 +0600

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,18 +1,4 @@
-$(function() {
-    // Owl Carousel
-    var owl = $(".owl-carousel");
-    owl.owlCarousel({
-        items:2,
-        loop:true,
-        nav:true,
-        dots:true,
-        autoplay:true,
-        autoplaySpeed:1000,
-        smartSpeed:1500,
-        autoplayHoverPause:true
-    });
-  });
-  const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navLinks = document.getElementsByClassName('links-container')[0]
 const navL = document.querySelectorAll('links-container li') ;
 toggleButton.addEventListener('click', () => {
